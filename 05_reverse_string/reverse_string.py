6b50abea2e05538e249ba1fa5ffdefca3be78a94
Sellers Crisp <sellershcrisp@gmail.com>
2020-11-01 19:07:42 -0600

    Reverse string function is working

--- 05_reverse_string/reverse_string.py
+++ 05_reverse_string/reverse_string.py
@@ -1,9 +1,11 @@
 def reverse_string(phrase):
     """Reverse string,
 
-        >>> reverse_string('awesome')
-        'emosewa'
+    >>> reverse_string('awesome')
+    'emosewa'
 
-        >>> reverse_string('sauce')
-        'ecuas'
+    >>> reverse_string('sauce')
+    'ecuas'
     """
+
+    return phrase[::-1]
